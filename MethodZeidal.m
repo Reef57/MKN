ebc688d0475fbfe730f4fc08d165d2eab2de91e9
Pavel <pavel20125@gmail.com>
2018-01-09 00:25:58 +1000

    Метод Зейдаля

---
+++ MethodZeidal.m
@@ -0,0 +1,45 @@
+A = [10 2 1 10; 1 10 2 12; 1 1 10 8]
+n = 3;
+x = zeros(1, n);
+x1 = zeros(1, n);
+e = 0.001;
+for i = 1:1:n
+    for k = 1:1:n+1
+       if i ~= k
+        A(i, k) = A(i, k)/A(i, i);
+       end 
+    end
+    A(i, i) = 0;
+end
+
+for k = 1:1:n % первичное преближение
+  xt = 0;
+  for j = 1:1:n
+    xt = xt + A(k, j)*x(1, j); 
+  end
+  x(1, k) = A(k, n+1) - xt; 
+end
+x
+
+for k = 1:1:n
+  xt = 0;
+  for j = 1:1:n
+    xt = xt + A(k, j)*x(1, j); 
+  end
+  x1(1, k) = A(k, n+1) - xt; 
+end 
+l = 1;
+
+while e < norm((x1 - x), 1)
+   x = x1;
+   for k = 1:1:n
+      xt = 0;
+      for j = 1:1:n
+         xt = xt + A(k, j)*x(1, j); 
+      end
+      x1(1, k) = A(k, n+1) - xt; 
+   end 
+   l = l + 1;
+end
+x
+l
