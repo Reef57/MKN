7c0a5b68b7aeb12bd59cc14a10970e775c553d3d
Pavel <pavel20125@gmail.com>
2018-01-09 12:25:29 +1000

    Correct

--- MethodZeidal.m
+++ MethodZeidal.m
@@ -19,27 +19,20 @@ for k = 1:1:n % первичное преближение
   end
   x(1, k) = A(k, n+1) - xt; 
 end
-x
 
-for k = 1:1:n
-  xt = 0;
-  for j = 1:1:n
-    xt = xt + A(k, j)*x(1, j); 
-  end
-  x1(1, k) = A(k, n+1) - xt; 
-end 
 l = 1;
 
-while e < norm((x1 - x), 1)
-   x = x1;
-   for k = 1:1:n
+while e < norm((x - x1), 1)
+    x1 = x;
+    for k = 1:1:n % первичное преближение
       xt = 0;
       for j = 1:1:n
-         xt = xt + A(k, j)*x(1, j); 
+        xt = xt + A(k, j)*x(1, j); 
       end
-      x1(1, k) = A(k, n+1) - xt; 
-   end 
-   l = l + 1;
+      x(1, k) = A(k, n+1) - xt; 
+    end 
+    x1
+    l = l + 1;
 end
 x
 l
